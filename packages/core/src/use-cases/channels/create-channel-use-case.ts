f734977c6458106ad5f86d79fe821db10aaf5742
JohnPetros <joaopedro.nc@outlook.com>
2024-08-15 20:42:46 -0300

    ✨ use cases: create channel

--- packages/core/src/use-cases/channels/create-channel-use-case.ts
+++ packages/core/src/use-cases/channels/create-channel-use-case.ts
@@ -1,14 +1,13 @@
-import { Channel } from '@/domain/entities'
-import type { ChannelDto } from '@/dtos'
-import type { UseCase } from '@/interfaces/handlers'
-import type { ChannelsRepository } from '@/interfaces/repositories'
+import { Channel } from '#domain/entities'
+import type { UseCase } from '#interfaces/handlers'
+import type { ChannelsRepository } from '#interfaces/repositories'
+import type { ChannelDto } from '#dtos'
 
 export class CreateChannelUseCase implements UseCase<ChannelDto, void> {
   constructor(private readonly repository: ChannelsRepository) {}
 
-  async execute(channelDto: ChannelDto) {
-    const channel = Channel.create(channelDto)
-
-    this.repository.add(channel)
+  async execute(dto: ChannelDto) {
+    const channel = Channel.create(dto)
+    await this.repository.add(channel)
   }
 }
