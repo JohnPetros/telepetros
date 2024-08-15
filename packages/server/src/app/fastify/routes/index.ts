1e3f16ca32396954f203ee044b5757b5ae36e888
JohnPetros <joaopedro.nc@outlook.com>
2024-08-15 20:41:21 -0300

    🪨 constants: add env

---
+++ packages/server/src/app/fastify/routes/index.ts
@@ -0,0 +1,1 @@
+export { ChannelsRoutes } from './channels-routes'
---
+++ packages/server/src/constants/env.ts
@@ -0,0 +1,12 @@
+import { serverEnvSchema } from '@telepetros/zod-validation/schemas'
+
+const envValidation = serverEnvSchema.safeParse({
+  port: process.env.POST,
+  mode: process.env.MODE,
+})
+
+if (!envValidation.success) {
+  throw new Error('Invalid server env variables')
+}
+
+export const ENV = envValidation.data
