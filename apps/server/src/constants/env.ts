a63d78ac3f91178d1a8f42fda26e64502dcf85f1
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-18 21:01:10 +0000

    🪨 constants: add github client variables to env

--- apps/server/src/constants/env.ts
+++ apps/server/src/constants/env.ts
@@ -3,6 +3,8 @@ import { serverEnvSchema } from '@telepetros/validation/schemas'
 const envValidation = serverEnvSchema.safeParse({
   port: process.env.POST,
   mode: process.env.MODE,
+  githubClientId: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
 })
 
 if (!envValidation.success) {
