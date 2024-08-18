29baa6136ea4cefc2d6a692e0cac467c7e3a4ddd
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-18 20:55:35 +0000

    🪨 constants: add http status code

---
+++ packages/core/src/constants/http-status-cde.ts
@@ -0,0 +1,12 @@
+export const HTTP_STATUS_CODE = {
+  ok: 200,
+  created: 201,
+  redirect: 301,
+  badRequest: 400,
+  unauthorized: 401,
+  forbidden: 403,
+  notFound: 404,
+  conflict: 409,
+  preconditionFailed: 412,
+  serverError: 500,
+}
