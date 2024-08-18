9694a00fb201fbf2f93b09d62d8a5d3278147b0f
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-18 20:54:29 +0000

    📨 responses: add service

---
+++ packages/core/src/responses/service-response.ts
@@ -0,0 +1,34 @@
+type ServiceResponseProps<Data> = {
+  data?: Data
+  error?: typeof Error
+}
+
+export class ServiceResponse<Data = null> {
+  private readonly _data: Data | null
+  private readonly _error: typeof Error | null
+
+  constructor({ data, error }: ServiceResponseProps<Data>) {
+    this._data = data ?? null
+    this._error = error ?? null
+  }
+
+  throwError() {
+    if (this._error) new this._error()
+  }
+
+  get isSuccess() {
+    return this._error === null
+  }
+
+  get isFailure() {
+    return this._error !== null
+  }
+
+  get data(): Data {
+    if (this._data === null) {
+      throw new Error('Service response is error')
+    }
+
+    return this._data
+  }
+}
