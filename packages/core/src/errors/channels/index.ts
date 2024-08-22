668801a214a34a7d0b8cae3dc304ca4b1bc5440c
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-22 00:04:17 +0000

    🚨 errors: add channels errors

---
+++ packages/core/src/errors/channels/index.ts
@@ -0,0 +1,2 @@
+export { CreateChannelError } from './create-channel-error'
+export { ListChannelsByChatterError } from './list-channels-by-chatter-error'
--- packages/core/src/interfaces/repositories/channels-repository.ts
+++ packages/core/src/interfaces/repositories/channels-repository.ts
@@ -1,6 +1,6 @@
 import type { Channel } from '#domain/entities'
 
 export interface IChannelsRepository {
-  add(channel: Channel): Promise<void>
+  add(channel: Channel): Promise<Channel>
   findManyByChatterId(chatterId: string): Promise<Channel[]>
 }
