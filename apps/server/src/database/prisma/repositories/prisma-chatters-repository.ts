10d203b19b0caa8eab9a7c5a39c88efa4d71fee2
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-23 02:18:58 +0000

    🛜 api: add chatters routes

--- apps/server/src/database/prisma/repositories/prisma-chatters-repository.ts
+++ apps/server/src/database/prisma/repositories/prisma-chatters-repository.ts
@@ -25,8 +25,32 @@ export class PrismaChattersRepository implements IChattersRepository {
   async add(chatter: Chatter): Promise<void> {
     const prismaChatter = this.mapper.toPrisma(chatter)
 
-    // await prisma.chatter.create({
-    //   data: prismaChatter,
-    // })
+    await prisma.chatter.create({
+      data: prismaChatter,
+    })
+  }
+
+  async findManyByChatterId(chatterId: string): Promise<Chatter[]> {
+    const chatterChats = await prisma.chatterChat.findMany({
+      where: {
+        OR: [{ chatter_1_id: chatterId }, { chatter_2_id: chatterId }],
+      },
+      include: {
+        chatter_1: true,
+        chatter_2: true,
+      },
+    })
+
+    const chatters = []
+
+    for (const { chatter_1_id, chatter_1, chatter_2_id, chatter_2 } of chatterChats) {
+      if (chatter_1_id === chatterId) {
+        chatters.push(chatter_2)
+      } else if (chatter_2_id === chatterId) {
+        chatters.push(chatter_1)
+      }
+    }
+
+    return chatters.map(this.mapper.toDomain)
   }
 }
