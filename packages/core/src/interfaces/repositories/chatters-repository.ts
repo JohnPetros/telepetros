264849f83eb1cbb0ddd9364a897569da2d7709bf
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-23 02:15:44 +0000

    📑 interfaces: fetch channel

--- packages/core/src/interfaces/repositories/chatters-repository.ts
+++ packages/core/src/interfaces/repositories/chatters-repository.ts
@@ -2,5 +2,6 @@ import type { Chatter } from '#domain/entities'
 
 export interface IChattersRepository {
   findById(id: string): Promise<Chatter | null>
+  findManyByChatterId(chatterId: string): Promise<Chatter[]>
   add(chatter: Chatter): Promise<void>
 }
