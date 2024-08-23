32ff43d5f5d1961549346726a7a8835af6ed31f9
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-23 02:15:09 +0000

    🛜 api: add GetChannelController

--- apps/server/src/api/controllers/channels/index.ts
+++ apps/server/src/api/controllers/channels/index.ts
@@ -1,2 +1,3 @@
 export { CreateChannelController } from './create-cannel-controller'
+export { GetChannelController } from './get-channel-controller'
 export { ListChatterChannelsController } from './list-chatter-channels-controller'
