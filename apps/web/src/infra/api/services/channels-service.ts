52a1dec0a9261187153d529b7382d71e5c845bb3
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-23 02:18:05 +0000

    🛜 api: implements chatters service

--- apps/web/src/infra/api/services/channels-service.ts
+++ apps/web/src/infra/api/services/channels-service.ts
@@ -5,6 +5,16 @@ import { ServiceResponse } from '@telepetros/core/responses'
 
 export const ChannelsService = (apiClient: IApiClient): IChannelsService => {
   return {
+    async fetchChannel(chatterId: string) {
+      const response = await apiClient.get<ChannelDto>(`/channels/${chatterId}`)
+
+      if (response.isError) {
+        return new ServiceResponse({ error: ListChannelsByChatterError })
+      }
+
+      return new ServiceResponse({ data: response.body })
+    },
+
     async createChannel(name: string, ownerId: string) {
       const response = await apiClient.post<ChannelDto>('/channels', {
         name,
--- apps/web/src/infra/api/services/index.ts
+++ apps/web/src/infra/api/services/index.ts
@@ -1,10 +1,13 @@
 import { ENV } from '@/ui/constants'
 import { NextApiClient } from '../next/next-api-client'
 import { ChannelsService } from './channels-service'
+import { ChattersService } from './chatters-service'
 import { AuthService } from './auth-service'
 
 const apiClient = NextApiClient()
+
 apiClient.setBaseUrl(ENV.serverUrl)
 
 export const authService = AuthService(apiClient)
 export const channelsService = ChannelsService(apiClient)
+export const chattersService = ChattersService(apiClient)
