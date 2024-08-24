360fcabc2b8e06a0dadb46ba13285109aa83a9e1
JohnPetros <joaopedro.nc@outlook.com>
2024-08-23 22:36:52 -0300

    🖥️ hooks: handle channel creation on use chat tabs

--- apps/web/src/ui/components/layouts/chat/chat-tabs/use-chat-tabs.ts
+++ apps/web/src/ui/components/layouts/chat/chat-tabs/use-chat-tabs.ts
@@ -6,6 +6,8 @@ import { useApi } from '@/infra/api'
 import { CACHE } from '@/ui/constants/cache'
 import { useAuthContext } from '@/ui/contexts/auth-context'
 import { useCache } from '@/ui/hooks/use-cache'
+import { useNavigation } from '@/ui/hooks'
+import { ROUTES } from '@/ui/constants'
 
 type Tab = 'channels' | 'chatters'
 
@@ -13,34 +15,34 @@ export function useChatTabs() {
   const [selectedTab, setSelectedTab] = useState<Tab>('channels')
   const { channelsService, chattersService } = useApi()
   const { chatter } = useAuthContext()
+  const { navigateTo } = useNavigation()
 
   async function fetchChannels() {
     if (!chatter) return
 
-    const response = await channelsService.listChannelsByChatter(chatter.id)
+    const response = await channelsService.fetchChannelsListByChatter(chatter.id)
 
     if (response.isFailure) {
-      console.log(response.errorMessage)
     }
-    return response.data
+    return response.body
   }
 
   async function fetchChatters() {
     if (!chatter) return
 
-    const response = await chattersService.listChattersByChatter(chatter.id)
+    const response = await chattersService.fetchChattersListByChatter(chatter.id)
 
     if (response.isFailure) {
-      console.log(response.errorMessage)
+      console.log(response.error)
     }
-    return response.data
+    return response.body
   }
 
   function handleTabChange(tab: string) {
     setSelectedTab(tab as Tab)
   }
 
-  const { data: channels } = useCache({
+  const { data: channels, mutateCache: mutateChannelsCache } = useCache({
     key: CACHE.channelsList.key,
     fetcher: fetchChannels,
   })
@@ -50,14 +52,14 @@ export function useChatTabs() {
   })
 
   async function handleCreateChannel(channelName: string) {
-    if (!chatter) return
+    if (!chatter || !channels) return
 
     const response = await channelsService.createChannel(channelName, chatter.id)
 
-    console.log(response)
-
     if (response.isSuccess) {
-      console.log({ createdChannelDto: response.data })
+      const createdChannel = response.body
+      await mutateChannelsCache([...channels, createdChannel])
+      navigateTo(`${ROUTES.channel}/${createdChannel.id}/chat`)
       return
     }
   }
