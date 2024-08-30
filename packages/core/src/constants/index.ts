7f17883e612ca61618040c62a02058137919348d
JohnPetros <joaopedro.nc@outlook.com>
2024-08-29 21:52:25 -0300

    🪨 constants: add MAX_IMAGE_FILE_SIZE

--- packages/core/src/constants/index.ts
+++ packages/core/src/constants/index.ts
@@ -1,2 +1,3 @@
 export { EVENTS } from './events'
 export { HTTP_STATUS_CODE } from './http-status-code'
+export { MAX_IMAGE_FILE_SIZE } from './max-image-file-size'
---
+++ packages/core/src/constants/max-image-file-size.ts
@@ -0,0 +1,1 @@
+export const MAX_IMAGE_FILE_SIZE = 5_242_880 // 5 MB
