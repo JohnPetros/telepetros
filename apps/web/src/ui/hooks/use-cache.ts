10b97778d05143c9ce9140d3832c777c465e3b16
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-08-31 19:55:44 +0000

    🖥️ hooks: add dependencies to use cache hook props

--- apps/web/src/ui/hooks/use-cache.ts
+++ apps/web/src/ui/hooks/use-cache.ts
@@ -3,17 +3,29 @@ import useSWR from 'swr'
 type UseCacheProps<CacheData> = {
   key: string
   fetcher: () => Promise<CacheData | undefined>
+  dependencies?: unknown[]
+  isEnabled?: boolean
   initialData?: CacheData
 }
 
 export function useCache<CacheData>({
   key,
   fetcher,
+  dependencies,
   initialData,
+  isEnabled = true,
 }: UseCacheProps<CacheData>) {
-  const { data, error, isLoading, mutate } = useSWR(key, fetcher, {
-    fallbackData: initialData,
-  })
+  const dependenciesQuery = dependencies
+    ? dependencies.map((dependency, index) => `dep_${index + 1}=${dependency}`)
+    : ''
+
+  const { data, error, isLoading, mutate } = useSWR(
+    () => (isEnabled ? `${key}?${dependenciesQuery}` : null),
+    fetcher,
+    {
+      fallbackData: initialData,
+    },
+  )
 
   async function mutateCache(data: CacheData) {
     await mutate(data)
