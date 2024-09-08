0fb327cbcd482b71cbaa4bd915361c600bb339e6
João Pedro Carvalho <joaopedro.nc@outlook.com>
2024-09-08 17:44:00 +0000

    🐛 fix: dayjs utc handliing

--- packages/core/src/libs/datetime.ts
+++ packages/core/src/libs/datetime.ts
@@ -1,10 +1,14 @@
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
 
 import type { IDatetime } from '../interfaces/libs'
 import type { DateFormat } from '../types/date-format'
 
+dayjs.extend(utc)
+
 export class DayjsDatetime implements IDatetime {
   format(date: Date, dateFormat: DateFormat): string {
-    return dayjs(date).format(dateFormat)
+    const utcDate = dayjs.utc(date)
+    return utcDate.format(dateFormat)
   }
 }
